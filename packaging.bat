c9b8f8b16b05509567b0cbfd22fc298a32f29814
Ryo Kanbayashi <ryo.contact@gmail.com>
2024-10-13 19:46:05 +0900

    added packageing.bat (2)

--- packaging.bat
+++ packaging.bat
@@ -1,32 +1,36 @@
 @echo off
 
+REM 第1引数と第2引数を取得
 set "SRC_DIR=%~1"
 set "TARGET_DIR=%~2"
 
+REM 引数の確認
 if "%SRC_DIR%"=="" (
-    echo please pass 1st argumnt (source dir).
+    echo 第1引数（ソースディレクトリ）を指定してください。
     goto end
 )
 
 if "%TARGET_DIR%"=="" (
-    echo please pass 2nd argument (target dir).
+    echo 第2引数（ターゲットディレクトリ）を指定してください。
     goto end
 )
 
+REM ターゲットディレクトリを作成
 if not exist "%TARGET_DIR%" (
     mkdir "%TARGET_DIR%"
 )
 
-REM activate venc
+REM venvをアクティベート
 call "%SRC_DIR%\venv\Scripts\activate.bat"
 
-REM packaging .py files on source dir
+REM ソースディレクトリ内の.pyファイルをパッケージ化
 for %%f in ("%SRC_DIR%\*.py") do (
-    echo %%~nxf is packaging...
+    echo %%~nxf をパッケージ化しています...
     pyinstaller --onefile "%%f" --distpath "%TARGET_DIR%"
 )
 
-REM deactivate venv
+REM venvをディアクティベート
 deactivate
 
 :end
+
