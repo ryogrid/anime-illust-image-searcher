dcb2bdfcb13f90f37c9364c24d83cd8ba7d0673f
Ryo Kanbayashi <ryo.contact@gmail.com>
2024-10-17 16:12:29 +0900

    implementing command launcher: WIP.

---
+++ cmd_run.py
@@ -0,0 +1,27 @@
+import argparse
+import sys
+
+import tagging
+import genmodel
+import counttag
+
+def main() -> None:
+    parser: argparse.ArgumentParser = argparse.ArgumentParser()
+    parser.add_argument('command', nargs=1, help='command to run')
+    # dummy
+    parser.add_argument('--dir', nargs=1, help='')
+    # dummy
+    parser.add_argument('--dim', nargs=1, type=int, help='')
+    args: argparse.Namespace = parser.parse_args()
+
+    if args.command[0] == 'tagging':
+        tagging.main(sys.argv)
+    elif args.command[0] == 'genmodel':
+        genmodel.main(sys.argv)
+    elif args.command[0] == 'counttag':
+        counttag.main()
+    else:
+        print('Invalid command')
+        exit(1)
+
+main()
