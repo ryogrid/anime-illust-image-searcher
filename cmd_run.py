9d0cb89d85bce9252a57cc53c410e094fcf1fbde
Ryo Kanbayashi <ryo.contact@gmail.com>
2024-10-29 13:11:30 +0900

    Update cmd_run.py

--- cmd_run.py
+++ cmd_run.py
@@ -10,16 +10,11 @@ def main() -> None:
     parser.add_argument('command', nargs=1, help='command to run')
     # dummy
     parser.add_argument('--dir', nargs=1, help='')
-    # dummy
-    parser.add_argument('--dim', nargs=1, type=int, help='')
-    args: argparse.Namespace = parser.parse_args()
 
     if args.command[0] == 'tagging':
         tagging.main(sys.argv[2:])
     elif args.command[0] == 'genmodel':
         genmodel.main(sys.argv[2:])
-    elif args.command[0] == 'counttag':
-        counttag.main()
     else:
         print('Invalid command')
         exit(1)
